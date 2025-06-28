e88eabcc9c95d49ffa9280cafefd98c5c201fcfa
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2025-06-28 19:09:11 -0400

    Update gamebro.py
    Optimized sprite type checking
    Added hitbox attribute

--- gamebro.py
+++ gamebro.py
@@ -1,123 +1,33 @@
-from typing import Any, Self, Callable
-import uuid
-from uuid import UUID
-from ursina import Entity, color
+# -*- coding: utf-8 -*-
+from gamebro import Sprite, SpriteGroup, entitify
+from ursina import *
+import sys
+import os
+from ursina.prefabs.first_person_controller import FirstPersonController
 
-class EngineError(Exception):
-    """
-    Error for GameBro
-    """
+# Project: s
+# Created by GameBro Studio
+
+app: Ursina = Ursina(title="s")
+window.title = "s"
+if os.path.exists(os.path.join("assets", "icon.ico")):
+    window.icon = os.path.join("assets", "icon.ico")
+window.exit_button.visible = False
+window.borderless = True
+
+player = FirstPersonController(position=(0, 5, 0))
+player.gravity = 1
+
+platform: Entity = Entity(model='cube', color=color.green, scale=(10, 1, 10), position=(0, 0, 0), collider='box')
+
+bro: Sprite = Sprite(customdata={'x': 0, 'y': 3, 'visible': True, "hitbox": False, 'color': [255.0, 0.0, 0.0]}, name="bro")
+E_bro: Entity = entitify(bro)
+
+def input(key: str) -> None:
+    if key == "escape":
+        sys.exit()
+
+def update() -> None:
     pass
 
-class Sprite:
-    def __init__(self: Self, customdata: dict[Any] = None, **options) -> None:
-        """
-        A sprite, think a player, or a monster
-
-        args: 
-            customdata:
-                description: Custom data attached to your sprite
-                type: list[Any]
-        kwargs:
-            name: 
-                description: Sprite name
-                type: str
-        """
-        self.event_listeners: dict[str, Callable] = {}
-        self.customdata: dict[Any] = customdata or {}
-        self.id: UUID = uuid.uuid4()
-        self.name: str = options.get("name", f"Sprite-{str(self.id)[:8]}")
-    def __str__(self: Self) -> str:
-        """
-        Developer friendly way to view the sprite
-        """
-        if "str-view" in self.event_listeners:
-            self.event_listeners["str-view"](self)
-        return f"<Sprite \"{self.name}\" with customdata {self.customdata}>"
-    def addeventlistener(self: Self, event: str) -> Callable[[Callable], Callable]:
-        """
-        Decorator to add an event listener to the sprite
-
-        args:
-            event:
-                type: str
-                description: Event name
-        """
-        def decorator(callback: Callable) -> Callable:
-            if not callable(callback):
-                raise EngineError("Callback must be a callable function.")
-            self.event_listeners[event] = callback
-            return callback
-        return decorator
-class SpriteGroup:
-    def __init__(self: Self, *elements: Any) -> None:
-        """
-        A group of sprites
-        args:
-            Sprites:
-                type: Sprite
-        """
-        for i in elements:
-            if not isinstance(i, Sprite):
-                raise EngineError("All elements in a group must be of type Sprite.")
-        self.elements: list[Sprite] = elements
-    def __str__(self: Self) -> str:
-        """
-        Developer friendly way to view the sprite group
-        """
-        return f"<SpriteGroup with sprites: {self.elements}>"
-    def __getitem__(self: Self, key: int) -> Sprite:
-        """
-        Get an item from the group using a key
-        
-        args:
-            key:
-                type: int
-        """
-        try:
-            return self.elements[key]
-        except IndexError as e:
-            raise EngineError("Key does not exist in group.") from e
-    def get(self: Self, key: int) -> Sprite:
-        """
-        See: __getitem__
-        """
-        return self.__getitem__(key)
-    def remove(self: Self, sprite: Sprite) -> None:
-        """
-        Remove an item from the group using a key
-        
-        args:
-            sprite:
-                type: Sprite
-        """
-        try:
-            self.elements.remove(sprite)
-        except ValueError as e:
-            raise EngineError("Value is not present in group.")
-        if "group-remove" in sprite.event_listeners:
-            sprite.event_listeners["group-remove"](sprite, self)
-    def add(self: Self, sprite: Sprite) -> None:
-        """
-        Add an item from the group using a key
-        
-        args:
-            sprite:
-                type: Sprite
-                description: Sprite to add
-        """
-        if not isinstance(sprite, Sprite):
-            raise EngineError("All elements in a group must be of type Sprite.")
-        self.elements.append(sprite)
-        if "group-add" in sprite.event_listeners:
-            sprite.event_listeners["group-add"](sprite, self)
-
-def entitify(sprite: Sprite) -> Entity:
-    entity: Entity = Entity(model="cube")
-    if "x" in sprite.customdata:
-        entity.x = sprite.customdata["x"]
-    if "y" in sprite.customdata:
-        entity.y = sprite.customdata["y"]
-    if "color" in sprite.customdata and isinstance(sprite.customdata["color"], list) and len(sprite.customdata["color"]) == 3:
-        entity.color = color.rgb(*sprite.customdata["color"])
-    return entity
+app.run()
